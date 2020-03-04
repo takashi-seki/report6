2e81752ac5e9d6e47570f11670271ff8d0a75815
takashi <t.seki1213@softbank.ne.jp>
2020-03-04 04:17:00 +0000

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::enrivonment('production')){
-            \URL::forceSheme('https');
+        if (\App::environment('production')){
+            \URL::forceScheme('https');
         }
     }
 
